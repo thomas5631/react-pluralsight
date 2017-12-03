d132cbae3407d9b8f53f634bf908611182c91fb6
Thomas Lilley <thomaslilley1@live.co.uk>
2017-12-03 16:01:39 +0000

    completed async status and error handling

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import authorApi from '../api/mockAuthorApi';
+import { beginAjaxCall, ajaxCallError } from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -7,9 +8,11 @@ export function loadAuthorsSuccess(authors) {
 
 export function loadAuthors() {
   return dispatch => {
+    dispatch(beginAjaxCall());
     return authorApi.getAllAuthors().then( authors => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch( error => {
+      dispatch(ajaxCallError());
       throw(error);
     });
   };
