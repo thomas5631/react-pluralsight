b78c363b2e7eced49f71c96b626fa88880c8112d
Thomas Lilley <thomaslilley1@live.co.uk>
2018-07-06 18:59:17 +0100

    Add PORT env var, add semi colon

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import { loadCourses } from './actions/courseActions';
-import { loadAuthors } from './actions/authorActions'
+import { loadAuthors } from './actions/authorActions';
 import './styles/styles.css'; //Webpack can import styles too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
