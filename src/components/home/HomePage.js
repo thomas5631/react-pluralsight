332f944ad94f3b695adc1a43ec09c5fc2c5f0ef4
Thomas Lilley <tlilley@twig-world.com>
2018-07-09 13:22:38 +0100

    feat(bryan): add bryan name to homepage closes #7

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,6 +5,7 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
+        <h1>Hello Bryan</h1>
         <h1>Pluralsight Administration</h1>
         <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
